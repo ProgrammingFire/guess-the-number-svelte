c636e6ae114bee9b2e3fc40b90b0c260cfb34834
Nouman Rahman <techcode786@gmail.com>
2023-09-14 21:36:24 +0530

    Change card

--- src/components/GuessCard.svelte
+++ src/components/GuessCard.svelte
@@ -16,15 +16,15 @@
 
 <style>
 	.card {
-		@apply font-medium text-slate-900 py-3 px-5 shadow-md rounded-md;
+		@apply font-medium text-slate-900 py-3 px-5 shadow-md rounded-md border-2 border-slate-800;
 	}
 	.yellow {
-		@apply bg-yellow-100;
+		@apply bg-yellow-200;
 	}
 	.red {
-		@apply bg-red-100;
+		@apply bg-red-200;
 	}
 	.green {
-		@apply bg-green-100;
+		@apply bg-green-200;
 	}
 </style>
